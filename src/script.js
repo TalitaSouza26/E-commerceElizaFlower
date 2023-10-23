c2f2c2e9e6c3a41aebf2a2277d8d6ef2ea0ef2ef
DevTalita <taa.jesus09@gmail.com>
2023-10-22 21:56:21 -0300

    E-commerce ElizaFlower

--- src/script.js
+++ src/script.js
@@ -1,4 +1,33 @@
+let slideIndex = 1;
 
-document.querySelector('.menuMobile').addEventListener('click', function() {
-    document.querySelector('.navLinks').classList.toggle('active');
-  })
+function showSlides(direcao) {
+    let i;
+    const slides = document.getElementsByClassName("mySlides");
+
+    if (direcao > slides.length) {
+        slideIndex = 1;
+    }
+    if (direcao < 1) {
+        slideIndex = slides.length;
+    }
+
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+
+    slides[slideIndex - 1].style.display = "block";
+}
+
+function plusSlides(direcao) {
+    showSlides(slideIndex += direcao);
+}
+
+function jumpToSlide() {
+    const input = document.getElementById("slideIndex");
+    const value = parseInt(input.value);
+    if (!isNaN(value)) {
+        showSlides(slideIndex = value);
+    }
+}
+
+showSlides(slideIndex);
